da25a556de197e495e4162480c2f0e818130f8fd
kanghuijin <rkd8527@naver.com>
2024-01-25 21:15:39 +0900

    Java코드를 Spring으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,6 +8,37 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Java -> Spring
+ * */
+//리팩토링 후
+@Configuration //스프링 설정 정보 어노테이션
+public class AppConfig {
+
+    @Bean //스프링 컨테이너에 등록
+    public MemberService memberService() { //메서드명을 스프링 빈의 이름으로 사용
+        return new MemberServiceImpl(memberRepository()); //@Bean이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다
+    }
+
+    @Bean
+    public static MemberRepository memberRepository() { //애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있는 장점
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public static DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
 
 /**
  * AppConfig를 통해서 관심사를 확실하게 분리했다.
@@ -16,22 +46,23 @@ import hello.core.order.OrderServiceImpl;
  * AppConfig는 공연 기획자다.
  * AppConfig는 구성 영역
  * */
-public class AppConfig {
-
-    public MemberService memberService() {
-        return new MemberServiceImpl(memberRepository());
-    }
-
-    private static MemberRepository memberRepository() { //애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있는 장점
-        return new MemoryMemberRepository();
-    }
-
-    public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
-
-    private static DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
-        return new RateDiscountPolicy();
-    }
-}
+//리팩토링 전
+//public class AppConfig {
+//
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(memberRepository());
+//    }
+//
+//    private static MemberRepository memberRepository() { //애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있는 장점
+//        return new MemoryMemberRepository();
+//    }
+//
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//    }
+//
+//    private static DiscountPolicy discountPolicy() {
+////        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
+//    }
+//}
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,15 +7,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
-//        MemberService memberService = new MemberServiceImpl();
+        //리팩토링 전
+//        MemberService memberService = new MemberServiceImpl(); //의존관계가 많아지는 단점이 있다.
 //        OrderService orderService = new OrderServiceImpl();
+
+        //AppConfig로 메서드 호출만 할 수 있게 역할 분리
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        //Java -> Spring
+        //스프링 컨테이너에서 Bean을 찾아오는 방식
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
